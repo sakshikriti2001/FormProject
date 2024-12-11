4d42d4abfe995aca165eaa6eeba6e8216a123817
sakshikriti2001 <msakshi2k1@gmail.com>
2024-12-12 00:01:20 +0530

    form generation

--- src/main/java/com/NewLearning/ProjectSpring/Services/CoursesServices.java
+++
@@ -1,9 +0,0 @@
-package com.NewLearning.ProjectSpring.Services;
-
-import com.NewLearning.ProjectSpring.Entity.Course;
-
-import java.util.List;
-// use interface class here because to use loose coupling  that means we can easily change in service not need to change in controller.
-public interface CoursesServices {
-
-}
--- src/main/java/com/NewLearning/ProjectSpring/dao/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.NewLearning.ProjectSpring.dao;
-
-import org.apache.catalina.User;
-
-public class UserRepository{
-}
